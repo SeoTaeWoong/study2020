d0c20152b08f5fc280955f3610ed74d443c68501
gur4777 <na386475@gmail.com>
2020-01-17 21:55:16 +0900

    "2020-01-17 공격모션"

--- study/src/galaga/GalagaCanvas.java
+++ study/src/galaga/GalagaCanvas.java
@@ -1,38 +1,60 @@
 package galaga;
 
 import java.awt.Canvas;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageProducer;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.ImageIcon;
 
 
 public class GalagaCanvas extends Canvas implements Runnable{
 	
-	int x1=0;
-	int y1=0;
-	int x2=5600;
-	int y2=1400;
+	int x1=500;
+	int y1=500;
+	int x2=6100;
+	int y2=1900;
+	
+	int atk1;
+	int atk2;
 	int cnt;
 	int cnt1;
-	Image imageB;
-	Image imageF;
+	Image imageB = getToolkit().getDefaultToolkit().getImage("image/space1.jpg");;
+	Image imageF = getToolkit().getDefaultToolkit().getImage("image/F-14.gif");;
+	Image imageA = getToolkit().getDefaultToolkit().getImage("image/15.gif");;
+	Image img;
+	Graphics gg;
+	private List<GalagaAttackDTO> list;
 	GalagaView gv;
 	Thread t;
 	
 	public GalagaCanvas(GalagaView gv) {
+		list = new ArrayList<GalagaAttackDTO>();
 		this.gv = gv;
 		
 	}
 	
 	@Override
 	public void update(Graphics g) {
-		
-		imageF = getToolkit().getDefaultToolkit().getImage("image/F-14.gif");
-		imageB = getToolkit().getDefaultToolkit().getImage("image/space1.jpg");
+		paint(g);
+	}
+	public void paint(Graphics g) {
+		img=createImage(1000,600);
+		gg = img.getGraphics();
+		buffering(gg);
+		if(cnt==0) {
+			repaint();
+			cnt++;
+			System.out.println(cnt);
+		}
+		g.drawImage(img, 0, 0, null);
+	}
+	private void buffering(Graphics gg) {
 		t = new Thread(this);
 		if(cnt1==0) {
 			t.start();
@@ -40,41 +62,44 @@ public class GalagaCanvas extends Canvas implements Runnable{
 		}
 		
 		
-		g.drawImage(imageB, 0, 0, 5600, 1400,x1,y1,x2,y2,this);
-		//g.drawImage(imageF, 0,0,80,50,0,0,150,75,this);
-		g.drawImage(imageF, gv.getX1(),gv.getY1(),gv.getX2(),gv.getY2(),0,0,150,75,this);
-		//
-		paint(g);
-	}
-	public void paint(Graphics g) {
-		//g.drawImage(imageB, x1, y1, this);
-		if(cnt==0) {
-			repaint();
-			cnt++;
-			System.out.println(cnt);
+		gg.drawImage(imageB, 0, 0, 5600, 1400,x1,y1,x2,y2,this);
+		gg.drawImage(imageF, gv.getX1(),gv.getY1(),gv.getX2(),gv.getY2(),0,0,150,75,this);
+		if(gv.isgAtk()) {
+			list.add(gv.getaDTO());
+		}
+		for(GalagaAttackDTO dto : list) {
+			gg.drawImage(imageA, dto.x, dto.y, dto.x+30, dto.y+10, 0, 0, 20, 10, this);
 		}
 	}
 	
 	@Override
 	public void run() {
-		synchronized(GalagaCanvas.class) {
-			while(true) {
-				x1+=10;
-				x2+=10;
-				if(x1 > 4000) {
+		while(true) {
+			
+			try {
+				x1+=1;
+				x2+=1;
+				if(x1 > 4700) {
 					x1=0; x2=5400;
 				}
-				try {
-					Thread.sleep(50);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				repaint();
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
+			repaint();
+			for(GalagaAttackDTO dto : list) {
+				dto.x+=+3;
+			}
+
+			
+			gv.setX1(gv.getX1()+gv.getMoveX1());
+			gv.setY1(gv.getY1()+gv.getMoveY1());
+			gv.setX2(gv.getX2()+gv.getMoveX2());
+			gv.setY2(gv.getY2()+gv.getMoveY2());
+			
 		}
-		
-		
-		
+			
 	}
+		
 	
 }
--- study/src/galaga/GalagaView.java
+++ study/src/galaga/GalagaView.java
@@ -7,6 +7,7 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -15,19 +16,23 @@ import javax.swing.JPanel;
 
 public class GalagaView extends JFrame implements KeyListener{
 	
-		GalagaCanvas can;
-		GalagaAttack gAtk;
-		private int x1 = 0;
-		private int y1 = 0;
-		private int x2 = 80;
-		private int y2 = 50;
+		private GalagaCanvas can;
+		private boolean gAtk;
+		private int x1 = 50;
+		private int y1 = 50;
+		private int x2 = 130;
+		private int y2 = 100;
+		private int moveX1 = 0;
+		private int moveY1 = 1;
+		private int moveX2 = 0;
+		private int moveY2 = 1;
 		private boolean keyUp, keyDown, keyLeft, keyRight;
+		private GalagaAttackDTO aDTO;
 		//0,0,80,50,0,0,150,75
 		
 		public GalagaView() {
 			
 			can= new GalagaCanvas(this);
-			gAtk = new GalagaAttack();
 			
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			setBounds(600,200,1000,600);
@@ -35,56 +40,63 @@ public class GalagaView extends JFrame implements KeyListener{
 //			can.setBackground(Color.black);
 			add(can);
 
-			Thread move = new Thread(can);
-			move.start();
-			
 			setVisible(true);
 			addKeyListener(this);
 			
-			
 		}
 		
-	
-
-
 		@Override
 		public void keyTyped(KeyEvent e) {
 			// TODO Auto-generated method stub
 			
 		}
 
-
-
 		@Override
 		public void keyPressed(KeyEvent e) {
+			
+			if(e.getKeyCode()== KeyEvent.VK_SPACE) {
+				gAtk = true;
+				aDTO = new GalagaAttackDTO(x2+10,y2-20);
+			}
 
 			if(e.getKeyCode()== KeyEvent.VK_LEFT) {
 				keyLeft = true;
-				if(x1!=-20) { x1-=10; x2-=10;}
+				if(x1!=-20) {
+					x1-=5; 
+					x2-=5; 
+					moveX1=-6; moveX2=-6;
+					moveY1=0; moveY2=0;
+				}
 			}else if(e.getKeyCode()== KeyEvent.VK_RIGHT) {
 				keyRight = true;
-				if(x2!=1000) {x1+=10; x2+=10;}
+				if(x2!=1000) {
+					x1+=5; x2+=5; 
+					moveX1=6; moveX2=6;
+					moveY1=0; moveY2=0;
+				}
 			}else if(e.getKeyCode()== KeyEvent.VK_UP) {
 				keyUp = true;
-				if(y1!=0) {y1-=10; y2-=10;}
+				if(y1!=0) {
+					y1-=5; y2-=5; 
+					moveY1=-6; moveY2=-6;
+					moveX1=0; moveX2=0;
+				}
 			}else if(e.getKeyCode()== KeyEvent.VK_DOWN) {
 				keyDown = true;
 				System.out.println(y2);
-				if(y2!=570) {y1+=10; y2+=10;}
+				if(y2!=570) {
+					y1+=5; y2+=5; 
+					moveX1=0; moveX2=0;
+					moveY1=6; moveY2=6;
+				}
 			}
 			
-//			if(e.getKeyCode()== KeyEvent.VK_SPACE) {
-//				Thread atk = new Thread(gAtk);
-//				
-//				atk.start();
-//			}
+			
 			
 			keyProcess();
 			can.repaint();
 			
 		}
-
-		
 
 		@Override
 		public void keyReleased(KeyEvent e) {
@@ -96,90 +108,114 @@ public class GalagaView extends JFrame implements KeyListener{
 				keyUp = false;
 			}else if(e.getKeyCode()== KeyEvent.VK_DOWN) {
 				keyDown = false;
-			}if(e.getKeyCode()== KeyEvent.VK_SPACE) {
-				
+			}
+			if(e.getKeyCode()== KeyEvent.VK_SPACE) {
+			//	gAtk = false;
 			}
 		}
 		
 		public void keyProcess() {
 			if(keyUp && keyLeft) {
 				if(x1>0 && y1>0) {
-					x1-=10; x2-=10;
-					y1-=10; y2-=10;
+					x1-=5; x2-=5;
+					y1-=5; y2-=5;
+					moveX1=-6; moveX2=-6;
+					moveY1=-6; moveY2=-6;
 				}
 			}else if(keyUp && keyRight) {
 				if(x2<1000 && y1>0) {
 					System.out.println(x1);
 					System.out.println(y1);
-					x1+=10; x2+=10;
-					y1-=10; y2-=10;
+					x1+=5; x2+=5;
+					y1-=5; y2-=5;
+					moveX1=6; moveX2=6;
+					moveY1=-6; moveY2=-6;
 				}
 			}else if(keyDown && keyLeft) {
-				if(x1>0 && y2<570) {
-					x1-=10; x2-=10;
-					y1+=10; y2+=10;
+				if(x1>0 && y2<560) {
+					x1-=5; x2-=5;
+					y1+=5; y2+=5;
+					moveX1=-6; moveX2=-6;
+					moveY1=6; moveY2=6;
 				}
 			}else if(keyDown && keyRight) {
-				if(x2<1000 && y2<570) {
-					x1+=10; x2+=10;
-					y1+=10; y2+=10;
+				if(x2<1000 && y2<560) {
+					x1+=5; x2+=5;
+					y1+=5; y2+=5;
+					moveX1=6; moveX2=6;
+					moveY1=6; moveY2=6;
 				}
 			}
+			
+			
 		}
 
 		public int getX2() {
 			return x2;
 		}
 
-
-
 		public void setX2(int x2) {
 			this.x2 = x2;
 		}
-
-
 
 		public int getX1() {
 			return x1;
 		}
 
-
-
 		public void setX1(int x1) {
 			this.x1 = x1;
 		}
-
-
 
 		public int getY1() {
 			return y1;
 		}
 
-
-
 		public void setY1(int y1) {
 			this.y1 = y1;
 		}
-
-
 
 		public int getY2() {
 			return y2;
 		}
 
-
-
 		public void setY2(int y2) {
 			this.y2 = y2;
 		}
 		
-		
-	
-		
+		public int getMoveY1() {
+			return moveY1;
+		}
+
+		public int getMoveX2() {
+			return moveX2;
+		}
+
+		public int getMoveY2() {
+			return moveY2;
+		}
+
+		public int getMoveX1() {
+			return moveX1;
+		}
+
+		public boolean isgAtk() {
+			return gAtk;
+		}
+
+		public void setgAtk(boolean gAtk) {
+			this.gAtk = gAtk;
+		}
+
 		public static void main(String[] args) {
 			new GalagaView();
 		}
 
+		public GalagaAttackDTO getaDTO() {
+			return aDTO;
+		}
 
+		public void setaDTO(GalagaAttackDTO aDTO) {
+			this.aDTO = aDTO;
+		}
 		
 }
